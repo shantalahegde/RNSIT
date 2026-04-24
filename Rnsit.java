7be32412b953501c2ece4e2071d6b0d85c4cded2
shantalahegde <ghshan4002@gmail.com>
2026-04-24 15:09:48 +0530

    I updated this program

--- Rnsit.java
+++ Rnsit.java
@@ -1,5 +1,8 @@
 class Display{
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Welcome to RNSIT");
+        System.out.println("Java Programming");
+        System.out.println("Have a nice dat!!!");
     }
 }
